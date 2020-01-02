44d1a854ee46d30cceaea92784689117a4437327
FayeLiu <lydia.liu@sinobot.com.cn>
2020-01-02 18:37:24 +0800

    ww

--- src/test/java/popsugar/selenium/base/SelectDriver.java
+++ src/test/java/popsugar/selenium/base/SelectDriver.java
@@ -7,10 +7,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class SelectDriver {
     public WebDriver selectDriver(String browser){
         if(browser.equals("chrome")){
-            System.setProperty("webdriver.chrome.marionette", "E:\\\\dependency pac\\\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.marionette", "E:\\Program Files\\chromedriver\\chromedriver.exe");
             return new ChromeDriver();
         }else{
-            System.setProperty("webdriver.firefox.marionette","E:\\\\dependency pac\\\\chromedriver.exe");
+            System.setProperty("webdriver.firefox.marionette","E:\\\\Program Files\\\\chromedriver\\\\chromedriver.exe");
             return new FirefoxDriver();
         }
 
